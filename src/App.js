6c16304e1a70b5859703a4556e888ae3e5100782
Oleksa <oleksa.antsupov@gmail.com>
2023-03-21 15:20:13 +0200

    add function App()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PMG weather app</h1>
+      <div className="container">
+        <div className="left-panel">
+          <input id="search"></input>
+          <button>search</button>
+        </div>
+
+        <div className="right-panel">
+          <h1>City Name</h1>
+          <h2>Temperature in °C</h2>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
