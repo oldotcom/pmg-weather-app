a18fd624842e8209f6e3573d93f4dd9a5122ab67
Oleksa <oleksa.antsupov@gmail.com>
2023-03-21 15:36:53 +0200

    add getResource

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@
 import "./styles.css";
 
 export default function App() {
+
+  const getResource = async (url) => {
+    let res = await fetch(url);
+
+    const errorMessage = () => {
+      alert("Enter correct location name, please ;)");
+    };
+
+    if (!res.ok) {
+      errorMessage();
+    }
+
+    return await res.json();
+  }
+
   return (
     <div className="App">
       <h1>PMG weather app</h1>
